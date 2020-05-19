4bc2dcfe4314ea4b0d01ad7115afaf12c9c18153
Dmitrii <dmitrii.stukalov@list.ru>
2020-05-19 23:59:32 +0300

    Solution

--- src/main/java/ru/ifmo/collections/SortedSet.java
+++ src/main/java/ru/ifmo/collections/SortedSet.java
@@ -1,36 +1,58 @@
 package ru.ifmo.collections;
 
-import java.util.AbstractSet;
-import java.util.Comparator;
+import java.util.*;
 
 /**
  * Represents sorted set of unique values.
  * create() returns a SortedSet instance with natural ordering. (i.e. from smallest to largest in case of integer numbers)
  * from() is used to create a SortedSet instance with given Comparator.
  * Instances of a class can be created using only these two methods above.
- *
+ * <p>
  * This class should not be abstract and should be capable of adding/removing single/multiple elements.
  * It has two more methods: getSorted() and getReversed() which return an array of set contents in forward and reverse order respectively.
- *
+ * <p>
  * NB! This class must have only map(s) as an internal data structure(s).
  *
  * @param <T> set contents type
  */
-public abstract class SortedSet<T> extends AbstractSet<T> {
-    // private final Map<???, ???> contents; TODO decide Map implementation and key/value types. "???" is used just as an example
+public class SortedSet<T> extends AbstractSet<T> {
+    private static final Object obj = new Object();
+    private final Map<T, Object> contents;
+
+    private SortedSet(TreeMap<T, Object> map) {
+        this.contents = map;
+    }
+
     public static <T> SortedSet<T> create() {
-        throw new UnsupportedOperationException(); // TODO implement
+        return new SortedSet<>(new TreeMap<>());
     }
 
     public static <T> SortedSet<T> from(Comparator<T> comparator) {
-        throw new UnsupportedOperationException(); // TODO implement
+        return new SortedSet<>(new TreeMap<>(comparator));
     }
 
-    public T[] getSorted() {
-        throw new UnsupportedOperationException(); // TODO implement
+    public List<T> getSorted() {
+        return new ArrayList<>(contents.keySet());
     }
 
-    public T[] getReversed() {
-        throw new UnsupportedOperationException(); // TODO implement
+    public List<T> getReversed() {
+        ArrayList<T> list = new ArrayList<>(contents.keySet());
+        Collections.reverse(list);
+        return list;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return contents.put(t, obj) == null;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return contents.keySet().iterator();
+    }
+
+    @Override
+    public int size() {
+        return contents.size();
     }
 }
